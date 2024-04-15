5826478048ca7f4023c37cbb8fb3d3c2a49d3d1b
delacruzbrandon <dcbrh.ph@gmail.com>
2024-04-15 16:27:12 +0800

    Updated end point

--- app/src/main/java/com/clutchit/emotionalintelligencecoexam/util/Constants.kt
+++ app/src/main/java/com/clutchit/emotionalintelligencecoexam/util/Constants.kt
@@ -3,5 +3,5 @@ package com.clutchit.emotionalintelligencecoexam.util
 object Constants {
 
     const val BASE_URL = "https://file.notion.so"
-    const val CHAPTER_LIST_END_POINT = "/f/f/e430ac3e-ca7a-48f9-804c-8fe9f7d4a267/3e5dda4e-0410-43be-b5a3-98b203be1a7e/response_1709543815894.json?id=f793d055-824e-4e22-b80f-5d79a970b13d&table=block&spaceId=e430ac3e-ca7a-48f9-804c-8fe9f7d4a267&expirationTimestamp=1713168000000&signature=hnkD0Ao8_ZdHSZayXkENWIxMBg254o3FXy4hdxkbHbQ&downloadName=response_1709543815894.json"
+    const val CHAPTER_LIST_END_POINT = "/f/f/e430ac3e-ca7a-48f9-804c-8fe9f7d4a267/3e5dda4e-0410-43be-b5a3-98b203be1a7e/response_1709543815894.json?id=f793d055-824e-4e22-b80f-5d79a970b13d&table=block&spaceId=e430ac3e-ca7a-48f9-804c-8fe9f7d4a267&expirationTimestamp=1713261600000&signature=zQbfmMDYRwlKWrW0GHSPaiIwVQTXz9IfccODHf5_m-k&downloadName=response_1709543815894.json"
 }
